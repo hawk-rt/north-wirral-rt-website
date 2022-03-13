93e5463bd7e92d8f197d16899ddca796ad9a5a07
Chris Nolan <chris@leytech.co.uk>
2022-03-13 21:04:53 +0000

    Install bootstrap-icons

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,6 +7,7 @@ mix.setPublicPath('source/assets/build');
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/css/main.scss', 'css')
+    .copy('node_modules/bootstrap-icons/font/fonts', 'source/assets/build/css/fonts')
     .options({
         processCssUrls: false,
     })
