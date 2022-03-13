c51d54eef2a852e0db960445491b22d881c85551
Chris Nolan <chris@leytech.co.uk>
2022-03-13 17:00:14 +0000

    Replace tailwind with bootstrap5

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,9 @@
 @extends('_layouts.main')
 
 @section('body')
-<div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
-</div>
+    <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+        <h1>Hello world!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <button class="btn btn-primary">Get Started</button>
+    </div>
 @endsection
