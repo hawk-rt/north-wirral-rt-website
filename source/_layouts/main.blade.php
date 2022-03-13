88cb1ea1b4723be5ab069ac090e935537678673b
Chris Nolan <chris@leytech.co.uk>
2022-03-13 21:03:30 +0000

    Initial layout based on startbootstrap-modern-business

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -2,14 +2,20 @@
 <html lang="{{ $page->language ?? 'en' }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="canonical" href="{{ $page->getUrl() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="{{ $page->description }}">
         <title>{{ $page->title }}</title>
+        <link rel="canonical" href="{{ $page->getUrl() }}">
+        <link rel="icon" href="/favicon.png" />
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </head>
-    <body>
-        @yield('body')
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+            @include('_partials.navigation')
+            @include('_partials.header')
+            @yield('body')
+            @include('_partials.footer')
+        </main>
     </body>
 </html>
