254023f446a0905f5d2b5968e6123e5f90220871
Chris Nolan <chris@leytech.co.uk>
2022-03-14 20:46:56 +0000

    New header and route modals for santa sleigh page

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,1 +1,17 @@
 import "bootstrap"
+
+var exampleModal = document.getElementById('santaRouteModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    var button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    var image = button.getAttribute('data-bs-image')
+    var title = button.getAttribute('data-bs-title')
+    // Update the modal's content.
+    var modalTitle = exampleModal.querySelector('.modal-title')
+    var modalImage = exampleModal.querySelector('.modal-body img')
+
+    modalTitle.textContent = title + ' Santa Sleigh Route'
+    modalImage.alt = title + ' Santa Sleigh Route'
+    modalImage.src = '/assets/images/santa-routes/' + image
+})
--- source/santa-sleigh.blade.php
+++ source/santa-sleigh.blade.php
@@ -2,12 +2,13 @@
 
 @section('body')
     <!-- Header-->
-    <header class="py-5">
+    <header class="bg-light py-5">
         <div class="container px-5">
-            <div class="row justify-content-center">
-                <div class="col-lg-8">
-                    <div class="text-center my-5">
-                        <h1 class="fw-bolder mb-3">Our famous Santa Sleigh, a magical experience for everyone to enjoy</h1>
+            <div class="row gx-5 align-items-center justify-content-center">
+                <div class="col-xl-5 col-xxl-6 d-block text-center"><img class="img-fluid rounded-3 my-5" src="/assets/images/photos/santa-sleigh-1.jpg" alt="Hoylake and West Kirby Santa Sleigh" /></div>
+                <div class="col-lg-8 col-xl-7 col-xxl-6">
+                    <div class="my-5 text-center text-xl-start">
+                        <h1 class="fw-bolder mb-2">Our famous Santa Sleigh, a magical experience for all</h1>
                         <p class="lead fw-normal text-muted mb-4">Every year we bring Santa to the streets of Greasby, Hoylake, Newton, Meols, and West Kirby to raise money for good causes and to bring a smile to the faces of the young children in our community.</p>
                         <p class="lead fw-normal text-muted mb-4">This is always a really special event for us. We love to see the community out on the streets and sharing this magical experience.</p>
                     </div>
@@ -16,7 +17,7 @@
         </div>
     </header>
     <!-- Santa sleigh routes section-->
-    <section class="py-5 bg-light">
+    <section class="py-5 bg-white">
         <div class="container px-5 my-5">
             <div class="row gx-5 justify-content-center">
                 <div class="col-lg-8 col-xl-6">
@@ -31,12 +32,12 @@
                 @foreach($santaRoutes as $route)
                     <div class="col-lg-4 mb-5">
                         <div class="card h-100 shadow border-0">
-                            <img class="card-img-top" src="/assets/images/santa-routes/{{ $route->image }}" alt="..." />
+                            <img class="card-img-top" src="/assets/images/santa-routes/{{ $route->image }}" alt="{{ $route->title }} Santa Sleigh Route" />
                             <div class="card-body p-4">
                                 @if ($route->pill)
                                 <div class="badge bg-primary bg-gradient rounded-pill mb-2">{{ $route->pill }}</div>
                                 @endif
-                                <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-3">{{ $route->title }}</h5></a>
+                                <a class="text-decoration-none link-dark stretched-link" href="#" data-bs-toggle="modal" data-bs-target="#santaRouteModal" data-bs-image="{{ $route->image }}" data-bs-title="{{ $route->title }}"><h5 class="card-title mb-3">{{ $route->title }}</h5></a>
                                 <p class="card-text mb-0">{{ $route->text }}</p>
                             </div>
                             <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
@@ -55,4 +56,17 @@
             </div>
         </div>
     </section>
+    <div class="modal fade" id="santaRouteModal" tabindex="-1" aria-labelledby="santaRouteModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="santaRouteModalLabel">Santa Route</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src="" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
