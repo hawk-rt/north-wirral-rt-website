3effdca3be5ffb8c4b86595090b3ad7233fc0056
Chris Nolan <chris@leytech.co.uk>
2022-03-15 12:06:34 +0000

    Fix for modal JS error

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,15 +1,15 @@
 import "bootstrap"
 
-var exampleModal = document.getElementById('santaRouteModal')
-exampleModal.addEventListener('show.bs.modal', function (event) {
+var santaRouteModal = document.getElementById('santaRouteModal')
+santaRouteModal && santaRouteModal.addEventListener('show.bs.modal', function (event) {
     // Button that triggered the modal
     var button = event.relatedTarget
     // Extract info from data-bs-* attributes
     var image = button.getAttribute('data-bs-image')
     var title = button.getAttribute('data-bs-title')
     // Update the modal's content.
-    var modalTitle = exampleModal.querySelector('.modal-title')
-    var modalImage = exampleModal.querySelector('.modal-body img')
+    var modalTitle = santaRouteModal.querySelector('.modal-title')
+    var modalImage = santaRouteModal.querySelector('.modal-body img')
 
     modalTitle.textContent = title + ' Santa Sleigh Route'
     modalImage.alt = title + ' Santa Sleigh Route'
