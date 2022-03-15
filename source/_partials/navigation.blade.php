d4441bfa2881a60286827f40a85db30b0e8e55c3
Chris Nolan <chris@leytech.co.uk>
2022-03-15 12:11:52 +0000

    Remove home link in menu and attempt to make navbar better on small screens

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -1,11 +1,17 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light bg-primary">
     <div class="container px-5">
-        <a class="navbar-brand" href="/">{{ $page->title }}</a>
+        <a class="navbar-brand d-none d-md-block" href="/">
+            <img src="/favicon.png" alt="" width="24" height="24" class="d-inline-block align-text-top">
+            {{ $page->title }}
+        </a>
+        <a class="navbar-brand fs-6 d-md-none text-center mx-0 w-100" href="/">
+            <img src="/favicon.png" alt="" width="36" height="36" class="d-inline-block mb-1"><br/>
+            {{ $page->title }}
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
                 <li class="nav-item"><a class="nav-link" href="/join-us">Join Us</a></li>
                 <li class="nav-item"><a class="nav-link" href="/apply-for-funding">Apply for Funding</a></li>
