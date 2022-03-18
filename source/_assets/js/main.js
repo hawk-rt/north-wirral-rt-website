9ab731cb9c5caab48cd52fa98159ffbe8264b822
Chris Nolan <chris@leytech.co.uk>
2022-03-18 09:44:27 +0000

    Only import required JS from Bootstrap

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,4 +1,16 @@
-import "bootstrap"
+// Import just what we need
+
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
 
 var santaRouteModal = document.getElementById('santaRouteModal')
 santaRouteModal && santaRouteModal.addEventListener('show.bs.modal', function (event) {
