fe88710c6f9667b6ccb863b311f4883a1fb57199
Chris Nolan <chris@leytech.co.uk>
2022-03-18 09:26:07 +0000

    Tidy up responsiveness of Nav menu

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -9,7 +9,7 @@
             <img src="/favicon.png" alt="" width="36" height="36" class="d-inline-block mb-1"><br/>
             {{ $page->title }}
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <button class="navbar-toggler ms-auto me-auto ms-md-0 me-md-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link {{ $page->active('') }}" href="/">Home</a></li>
