8d06770e45900f3a62e2216de74a5eb5dc05daeb
Chris Nolan <chris@leytech.co.uk>
2023-11-30 12:33:07 +0000

    NWRT logo

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -1,15 +1,15 @@
 <!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+<nav class="navbar navbar-expand-xl navbar-light bg-primary">
     <div class="container px-3 px-md-5">
-        <a class="navbar-brand d-none d-md-block" href="/">
-            <img src="/favicon.png" alt="" width="24" height="24" class="d-inline-block align-text-top">
+        <a class="navbar-brand d-none d-lg-block" href="/">
+            <img src="/assets/images/nwrt_logo_dark_on_trans_200px.png" alt="" width="24" height="24" class="d-inline-block align-text-top">
             {{ $page->title }}
         </a>
-        <a class="navbar-brand fs-6 d-md-none text-center mx-0 w-100" href="/">
-            <img src="/favicon.png" alt="" width="36" height="36" class="d-inline-block mb-1"><br/>
+        <a class="navbar-brand fs-6 d-lg-none text-center mx-0 w-100" href="/">
+            <img src="/assets/images/nwrt_logo_dark_on_trans_200px.png" alt="" width="36" height="36" class="d-inline-block mb-1"><br/>
             {{ $page->title }}
         </a>
-        <button class="navbar-toggler ms-auto me-auto ms-md-0 me-md-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <button class="navbar-toggler ms-auto me-auto ms-lg-0 me-lg-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link {{ $page->active('') }}" href="/">Home</a></li>
