2f0e4758ba49fd1d7c718bc0f6d423397ee932ab
Chris Nolan <chris@leytech.co.uk>
2023-11-30 12:37:21 +0000

    Instagram link

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -15,6 +15,7 @@
             <div class="col-lg-4 mb-5 mb-lg-0">
                 <h4 class="text-uppercase mb-4">Around the Web</h4>
                 <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/northwirralroundtable"><i class="bi bi-facebook"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/northwirralroundtable"><i class="bi bi-instagram"></i></a>
                 <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/NorthWirralRT"><i class="bi bi-twitter"></i></a>
             </div>
             <!-- Footer About Text-->
